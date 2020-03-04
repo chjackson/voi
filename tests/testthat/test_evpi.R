36a14dd5b000223c5ef628ae70a0682598853e14
Christopher Jackson <chris.jackson@mrc-bsu.cam.ac.uk>
2020-03-04 17:53:40 +0000

    Added evpi test file

---
+++ tests/testthat/test_evpi.R
@@ -0,0 +1,4 @@
+test_that("EVPI",{
+    expect_equal(evpi(chemo_nb), 42.178, tolerance=0.01)
+    expect_equal(evpi(chemo_cea), c(1.02877281101973, 12.3534189284037, 42.1780795210652, 58.9895661745759, 40.7463368296667), tolerance=0.01)
+})
