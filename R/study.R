bd84380b17b54b6acdd8f835c0669b5cef5a769a
Christopher Jackson <chris.jackson@mrc-bsu.cam.ac.uk>
2020-09-03 09:54:30 +0100

    Add single-arm binary outcome study

--- R/study.R
+++ R/study.R
@@ -1,5 +1,18 @@
 ## Built-in study designs 
 
+datagen_binary <- function(inputs, n=100, poi){
+    nsim <- nrow(inputs)
+    data.frame(
+        X1 = rbinom(nsim, size=n, prob=inputs[,poi[1]])
+    )
+}
+
+likelihood_binary <- function(Y, inputs, n=100, poi){
+    loglik <-
+        dbinom(Y[,"X1"], size=n, inputs[,poi[1]], log=TRUE) 
+    exp(loglik)
+}
+
 datagen_trial_binary <- function(inputs, n=100, poi){
     nsim <- nrow(inputs)
     data.frame(
@@ -15,4 +28,4 @@ likelihood_trial_binary <- function(Y, inputs, n=100, poi){
     exp(loglik)
 }
 
-studies_builtin <- c("trial_binary")
+studies_builtin <- c("binary","trial_binary")
