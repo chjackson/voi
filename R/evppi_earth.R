bd81502d81706bda5c69e929b5c48dd5b91a2540
Christopher Jackson <chris.jackson@mrc-bsu.cam.ac.uk>
2020-09-03 09:53:42 +0100

    Allow interactions in earth default formula

--- R/evppi_earth.R
+++ R/evppi_earth.R
@@ -1,11 +1,14 @@
+
 ## TODO standard errors 
 
 fitted_earth <- function(y, inputs, poi, ...){
     opts <- list(...)
     earth_formula <- opts$earth_formula
-    if (is.null(earth_formula))
-        earth_formula <- "." 
-    earth_formula <- formula(sprintf("y ~ %s", earth_formula))
-    model <- earth::earth(formula=earth_formula, data = inputs[,poi,drop=FALSE])
+    if (is.null(earth_formula)) { 
+        model <- earth::earth(y=y, x=inputs[,poi,drop=FALSE], ...)
+    } else {
+        earth_formula <- formula(sprintf("y ~ %s", earth_formula))
+        model <- earth::earth(formula=earth_formula, data = inputs[,poi,drop=FALSE], ...)
+    }
     model$fitted
 }
