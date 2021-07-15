4e9d48f565cb7d24e1cc0f1d5224960490e12184
Chris Jackson <chris.jackson@mrc-bsu.cam.ac.uk>
2021-07-15 18:14:10 +0100

    Empty tests

--- tests/testthat/test_evppivar.R
+++ tests/testthat/test_evppivar.R
@@ -8,8 +8,10 @@ test_that("single-parameter variance-based EVPPI",{
 })
 
 test_that("single-parameter variance-based EVPPI, alternative GAM basis",{
+    expect_error({
     evppivar(chemo_nb[,1], chemo_pars, pars="pi1", gam_formula="s(pi1, bs='tp')")
     pi1 <- chemo_pars[,"pi1"]
     evppivar(chemo_nb[,1], pi1, gam_formula="s(pi1, bs='tp')")
+    }, NA)
 })
 
--- tests/testthat/test_evsivar.R
+++ tests/testthat/test_evsivar.R
@@ -1,6 +1,8 @@
 
 test_that("Variance-based EVSI with built-in study designs", {
+    expect_error({
     set.seed(1)
     evsivar(chemo_nb[,1], chemo_pars, study="trial_binary", pars=c("pi1", "pi2"), verbose=FALSE)
     evsivar(chemo_nb[,1], chemo_pars, study="binary", pars=c("pi1"), verbose=FALSE)
+    },NA)
 })
