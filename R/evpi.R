310ba5621c27a4967f3d4f0987e6be608ab12304
Chris Jackson <chris.jackson@mrc-bsu.cam.ac.uk>
2021-08-03 17:43:19 +0100

    Return data frame for EVPI with CEA format

--- R/evpi.R
+++ R/evpi.R
@@ -4,7 +4,7 @@
 ##'
 ##' @inheritParams evppi
 ##'
-##' @return The expected value of perfect information, either a single value, or a vector of values for different willingness-to-pay.
+##' @return The expected value of perfect information, either as a single value, or a data frame indicating the value for each willingness-to-pay.
 ##' 
 ##' @export
 evpi <- function(outputs,
@@ -22,6 +22,7 @@ evpi <- function(outputs,
             nb <- outputs$e * outputs$k[i]  -  outputs$c
             res[i] <- mean(apply(nb, 1, max)) - max(colMeans(nb))
         }
+        res <- data.frame(k = outputs$k, evpi = res)
     }
     res
 }
