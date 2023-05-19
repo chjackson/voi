b733a44361faf7b4d5e7e7c7e90f4c008d10e49a
Chris Jackson <chris.jackson@mrc-bsu.cam.ac.uk>
2023-05-19 17:44:39 +0100

    Book code

--- inst/book_misc/gp_graphs.r
+++ inst/book_misc/gp_graphs.r
@@ -13,22 +13,20 @@ qplot(y, x)
 # sets: names of POI?  why not do this outside ? it does some error checking. 
 # it removes any parameters that are constant.  Needs external warning 
 
-inputs <- remove_constant_linear_cols(inputs)
-
 xpred <- seq(min(x), max(x), by=0.001)
-mod <- gp(y, x, Xpred=xpred)
+mod <- voi:::gp(y, x, Xpred=xpred)
 
 # lower the first par to make less smooth
 # nu controls the extent of measurement error. 
 # Note this does not do any prediction outside the data points so the fit is jagged. 
 
-mod <- gp(y, x, hyper = c(1, 0.5637912), Xpred=xpred)
-mod <- gp(y, x, hyper = c(0.5, 0.5637912), Xpred=xpred)
+mod <- voi:::gp(y, x, hyper = c(1, 0.5637912), Xpred=xpred)
+mod <- voi:::gp(y, x, hyper = c(0.5, 0.5637912), Xpred=xpred)
 
-mod1 <- gp(y, x, hyper = c(1, 0.001), Xpred=xpred)
-mod2 <- gp(y, x, hyper = c(0.5, 0.001), Xpred=xpred)
-mod3 <- gp(y, x, hyper = c(0.1, 0.001), Xpred=xpred)
-mod4 <- gp(y, x, hyper = c(0.01, 0.001), Xpred=xpred)
+mod1 <- voi:::gp(y, x, hyper = c(1, 0.001), Xpred=xpred)
+mod2 <- voi:::gp(y, x, hyper = c(0.5, 0.001), Xpred=xpred)
+mod3 <- voi:::gp(y, x, hyper = c(0.1, 0.001), Xpred=xpred)
+mod4 <- voi:::gp(y, x, hyper = c(0.01, 0.001), Xpred=xpred)
 
 dat <- data.frame(y, x)
 datpred1 <- data.frame(x=xpred, fitted=mod1$pred, delta=1)
@@ -40,12 +38,13 @@ datpred$delta <- factor(datpred$delta)
 
 cols <- RColorBrewer::brewer.pal(4, "Greys")[2:4]
 
-pdf("Figures/04-evppi/gp_regression.pdf", width=6, height=3)  
+pdf("~/work/voibook/voibook/Figures/04-evppi/gp_regression.pdf", width=6, height=3)  
 ggplot(dat, aes(x=x, y=y)) +
   geom_line(data=datpred, aes(x=x, y=fitted, col=delta), lwd=1, alpha=0.8) + 
   scale_color_manual(breaks=levels(datpred$delta), values=cols) + 
-  coord_cartesian(ylim=c(0.9, 1.01)) +
+  coord_cartesian(ylim=c(0, 0.6)) +
   geom_point(size=2) + 
   xlab("Predictor") + ylab("Outcome") + 
-  labs(col=expression(delta))
+  labs(col=expression(delta)) +
+  theme_bw()
 dev.off()
