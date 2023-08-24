6d45eff966108fd0071773f83ee587e6d6f3436d
Chris Jackson <chris.jackson@mrc-bsu.cam.ac.uk>
2023-08-24 20:32:22 +0100

    Slow tests reinstated

--- tests/tests_slow/test_evppi.R
+++ tests/tests_slow/test_evppi.R
@@ -1,20 +1,3 @@
 pi1 <- "p_side_effects_t1"
 pi2 <- "p_side_effects_t2"
 rho <- "logor_side_effects"
-
-if (requireNamespace(dbarts,quietly=TRUE)) { 
-  test_that("EVPPI with BART",{
-    pars <- c(pi2,rho)
-    expect_equal(evppi(chemo_nb, chemo_pars, pars=pars, method="bart", nsim=1000)$evppi,
-                 324.2451, tol=1e-02)
-    expect_equal(evppi(chemo_nb, chemo_pars, pars=pars, method="bart", nsim=1000, ndpost=2000, se=TRUE)$evppi,
-                 324.0516, tol=1e-02)
-  })
-}
-
-test_that("Standard errors in the earth method",{
-  set.seed(1)
-  ev1 <- evppi(chemo_nb, chemo_pars, pars=pi2, method="earth", se=TRUE, nsim=100)
-  ev2 <- evppi(chemo_nb, chemo_pars, pars=pi2, method="earth", se=TRUE, nsim=500)
-  expect_lt(ev2$se[1], ev1$se[1])
-})
--- tests/testthat/test_evppi.R
+++ tests/testthat/test_evppi.R
@@ -52,6 +52,25 @@ if (requireNamespace("INLA",quietly=TRUE)) {
   })
 }
 
+if (requireNamespace("dbarts",quietly=TRUE)) { 
+  test_that("EVPPI with BART",{
+    skip_on_cran()
+    pars <- c(pi2,rho)
+    expect_equal(evppi(chemo_nb, chemo_pars, pars=pars, method="bart", nsim=1000)$evppi,
+                 324.2451, tol=1e-02)
+    expect_equal(evppi(chemo_nb, chemo_pars, pars=pars, method="bart", nsim=1000, ndpost=2000, se=TRUE)$evppi,
+                 324.0516, tol=1e-02)
+  })
+}
+
+test_that("Standard errors in the earth method",{
+  skip_on_cran()
+  set.seed(1)
+  ev1 <- evppi(chemo_nb, chemo_pars, pars=pi2, method="earth", se=TRUE, nsim=100)
+  ev2 <- evppi(chemo_nb, chemo_pars, pars=pi2, method="earth", se=TRUE, nsim=500)
+  expect_lt(ev2$se[1], ev1$se[1])
+})
+
 test_that("multi-parameter EVPPI, gam",{
     pars <- c(pi2,rho)
     expect_equal(evppi(chemo_nb, chemo_pars, pars=pars, method="gam")$evppi,
